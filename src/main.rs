96650efe03ed2ee91bf9a2859aea8a72a163fce1
vypxl <thomas@vypxl.io>
2023-12-15 21:02:37 +0900

    add gui via egui

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 use std::{
     error::Error,
+    path::PathBuf,
     thread::{self, JoinHandle},
 };
 
 use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
 
+use egui_file::FileDialog;
 use rust_audio_shenanigans::{
     effects::lowpass,
     instrument::*,
@@ -74,36 +76,36 @@ fn process_event(
         midly::TrackEventKind::Midi { message, .. } => match message {
             midly::MidiMessage::NoteOn { key, vel } => {
                 if vel.as_int() == 0 {
-                    println!("off");
+                    // println!("off");
                     trigger(key.as_int() as usize, ADSREvent::Release);
                     return;
                 }
-                println!("triggering: {key} {vel}");
+                // println!("triggering: {key} {vel}");
                 trigger(key.as_int() as usize, ADSREvent::Press(vel.as_int()));
             }
             midly::MidiMessage::NoteOff { key, .. } => {
                 // todo
                 trigger(key.as_int() as usize, ADSREvent::Release);
-                println!("off");
+                // println!("off");
             }
-            x => {
-                println!("midi: {:?}", x);
+            _x => {
+                // println!("midi: {:?}", _x);
             }
         },
         midly::TrackEventKind::Meta(meta) => match meta {
             midly::MetaMessage::Tempo(tempo) => {
                 *mspt = tempo.as_int() as f32 / (1.0 * tpb as f32);
-                println!("tempo: {}, tpb: {}, mspt: {}", tempo, tpb, mspt);
+                // println!("tempo: {}, tpb: {}, mspt: {}", tempo, tpb, mspt);
             }
-            midly::MetaMessage::TrackName(name) => {
-                println!("track name: {}", String::from_utf8_lossy(name));
+            midly::MetaMessage::TrackName(_name) => {
+                // println!("track name: {}", String::from_utf8_lossy(_name));
             }
-            x => {
-                println!("meta: {:?}", x);
+            _x => {
+                // println!("meta: {:?}", _x);
             }
         },
-        x => {
-            println!("other: {:?}", x);
+        _x => {
+            // println!("other: {:?}", _x);
         }
     }
 }
@@ -149,19 +151,150 @@ fn setup_streamer(sample_rate: u32, song: midly::Smf) -> (WaveStreamer, JoinHand
     (WaveStreamer::new(wave, sample_rate), handle)
 }
 
-fn main() -> Result<(), Box<dyn Error>> {
+struct Player {
+    stream: cpal::Stream,
+    _thread_handle: Option<JoinHandle<()>>,
+}
+
+impl Player {
+    fn new(stream: cpal::Stream, thread_handle: JoinHandle<()>) -> Self {
+        Self {
+            stream,
+            _thread_handle: Some(thread_handle),
+        }
+    }
+    fn play(&self) -> Result<(), Box<dyn Error>> {
+        self.stream.play()?;
+        Ok(())
+    }
+    fn stop(&self) -> Result<(), Box<dyn Error>> {
+        self.stream.pause()?;
+        Ok(())
+    }
+
+    // fn wait(&mut self) {
+    //     if let Some(h) = self._thread_handle.take() {
+    //         h.join().unwrap();
+    //     }
+    // }
+}
+
+fn play_song_from_file(fname: &str) -> Result<Player, Box<dyn Error>> {
     let (device, config) = setup_device()?;
 
-    let fname = std::env::args().nth(1).unwrap();
     let data = std::fs::read(fname)?;
     let smf = midly::Smf::parse(&data)?;
     let (streamer, handle) = setup_streamer(config.sample_rate.0, smf);
 
     let stream = setup_stream(&device, &config, streamer)?;
-    stream.play()?;
 
-    handle.join().unwrap();
-    // To not cut off the last notes
-    std::thread::sleep(std::time::Duration::from_secs(1));
+    Ok(Player::new(stream, handle))
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let fname = std::env::args().nth(1).unwrap_or_default();
+
+    open_gui(fname)?;
     Ok(())
+}
+
+fn open_gui(fname: String) -> Result<(), eframe::Error> {
+    let options = eframe::NativeOptions {
+        viewport: egui::ViewportBuilder::default().with_inner_size([320.0, 240.0]),
+        ..Default::default()
+    };
+
+    eframe::run_native(
+        "vypxl's Rust Audio Shenanigans",
+        options,
+        Box::new(|_| Box::new(App::new(fname))),
+    )
+}
+
+#[derive(Default)]
+struct App {
+    fname: Option<PathBuf>,
+    open_file_dialog: Option<FileDialog>,
+    player: Option<Player>,
+}
+
+impl App {
+    fn new(fname: String) -> Self {
+        let should_play = !fname.is_empty();
+        let mut ret = Self {
+            fname: Some(PathBuf::from(fname)),
+            ..Default::default()
+        };
+
+        if should_play {
+            ret.play();
+        }
+
+        ret
+    }
+
+    fn play(&mut self) {
+        if let Some(fname) = &self.fname {
+            let fname = fname.to_str().unwrap();
+            if let Some(h) = self.player.take() {
+                let _ = h.stop();
+            }
+            println!("Playing {}", fname);
+            let player = play_song_from_file(fname).unwrap();
+            let _ = player.play();
+            self.player = Some(player);
+        } else {
+            println!("No file selected!");
+        }
+    }
+}
+
+impl eframe::App for App {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.vertical_centered_justified(|ui| {
+                ui.heading("vypxl's Rust Audio Shenanigans");
+                ui.label("Choose a MIDI file to play:");
+                ui.horizontal(|ui| {
+                    let lab = ui.label("Path: ");
+                    let mut p: String = self
+                        .fname
+                        .clone()
+                        .unwrap_or_default()
+                        .into_os_string()
+                        .into_string()
+                        .unwrap_or_default();
+                    let edit = ui.text_edit_singleline(&mut p).labelled_by(lab.id);
+                    if edit.changed() {
+                        self.fname = Some(PathBuf::from(p));
+                    }
+
+                    if (ui.button("Choose File")).clicked() {
+                        let mut dialog = FileDialog::open_file(self.fname.clone());
+                        dialog.open();
+                        self.open_file_dialog = Some(dialog);
+                    }
+                });
+
+                if let Some(dialog) = &mut self.open_file_dialog {
+                    if dialog.show(ctx).selected() {
+                        if let Some(file) = dialog.path() {
+                            self.fname = Some(file.to_path_buf());
+                        }
+                    }
+                }
+
+                ui.horizontal(|ui| {
+                    if ui.button("Play").clicked() {
+                        self.play()
+                    }
+                    if ui.button("Stop").clicked() {
+                        if let Some(h) = self.player.take() {
+                            let _ = h.stop();
+                        }
+                    }
+                });
+            });
+        });
+    }
 }
